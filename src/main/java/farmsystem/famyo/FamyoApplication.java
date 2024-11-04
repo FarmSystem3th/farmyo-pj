d685a1c6de7b488c83ba9024cd66f82fddac9c7a
haeun1107 <haeun9394@gmail.com>
2024-11-04 18:31:43 +0900

    Feat: 프로젝트 초기 세팅

---
+++ src/main/java/farmsystem/famyo/FamyoApplication.java
@@ -0,0 +1,13 @@
+package farmsystem.famyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamyoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/farmsystem/famyo/FamyoApplicationTests.java
@@ -0,0 +1,13 @@
+package farmsystem.famyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
